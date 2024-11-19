3d80e20b8b402730a7b4caebeef20d1be7382121
TeemuTallskog <teemutallskog@gmail.com>
2024-11-20 01:38:20 +0200

    Add multiclip support
    Allow user to define multiple parts that should get clipped together.

--- clip/clip.py
+++ clip/clip.py
@@ -1,24 +1,19 @@
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.video.fx.all import crop
+from moviepy.video.compositing.concatenate import concatenate_videoclips
 import argparse
 import re
 
-def split_video(input_file, start_sec, end_sec, output_file, short=False):
-    """
-    Splits a video between two timestamps and saves the result.
-
-    Parameters:
-        input_file (str): Path to the input video file.
-        start_time (str): Start time in "HH:MM:SS" or "MM:SS" format.
-        end_time (str): End time in "HH:MM:SS" or "MM:SS" format.
-        output_file (str): Path to save the output video.
-    """
+def split_video(input_file, clips, output_file, short=False):
     try:
         video = VideoFileClip(input_file)
-        if start_sec < 0 or end_sec > video.duration or start_sec >= end_sec:
-            raise ValueError("Invalid timestamps. Ensure start < end and within video duration.")
+        for start, end in clips:
+            if start < 0 or end > video.duration or start >= end:
+                raise ValueError("Invalid timestamps. Ensure start < end and within video duration.")
+        
+        video_parts = [video.subclip(start,end) for start, end in clips]
 
-        trimmed_video = video.subclip(start_sec, end_sec)
+        trimmed_video = concatenate_videoclips(clips=video_parts)
 
         if short:
             video_width, video_height = trimmed_video.size
@@ -43,27 +38,30 @@ def split_video(input_file, start_sec, end_sec, output_file, short=False):
         video.close()
 
 def time_type(value):
-    pattern = r'\d+:\d+:\d+'
+    pattern = r'\d+:\d+:\d+\-\d+:\d+:\d'
     if not re.search(pattern, value):
         raise argparse.ArgumentTypeError(f"Invalid value: {value}")
-    parts = list(map(int, value.split(":")))
-    return sum(p * 60 ** i for i, p in enumerate(reversed(parts)))
+    def get_time_in_seconds(stamp):
+        parts = list(map(int, stamp.split(":")))
+        return sum(p * 60 ** i for i, p in enumerate(reversed(parts)))
+    start_end = value.split("-", 1)
+    start = get_time_in_seconds(start_end[0])
+    end = get_time_in_seconds(start_end[1])
+    return (start, end)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--path", type=str, required=True)
-    parser.add_argument("-s", "--start", type=time_type, required=True)
-    parser.add_argument("-e", "--end", type=time_type, required=True)
+    parser.add_argument("-c", "--clip", nargs='+', type=time_type, required=True, help="Provide time stamps in format HH:mm:ss-HH:mm:ss. eg. 0:1:22-0:1:30")
     parser.add_argument("-o", "--output", type=str, default="output.mp4")
     parser.add_argument("-yts", "--youtube-short", action="store_true")
     args = parser.parse_args()
     input_video = args.path
-    start = args.start
-    end = args.end 
+    clips = args.clip
     output_video = args.output 
     short = args.youtube_short
 
-    split_video(input_video, start, end, output_video, short)
+    split_video(input_video, clips, output_video, short)
 
 if __name__ == "__main__":
     main()
